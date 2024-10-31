08a21df0284489ef22600fbff6e7810862452d1c
Jia Jun Lee <lee19@live.cn>
2024-10-31 18:09:55 +0800

    add

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -6,9 +6,9 @@ echo '[info] Deploying ...'
 
 echo '[info] npm run start'
 # npm run start
-echo 'npm run start' &
+sleep 300 &
 sleep 1
 echo $! > .pidfile
-echo 'Current running pid is ' $!
+echo 'Current running pid is' $!
 
 echo '[info] Deploy Completed !'
